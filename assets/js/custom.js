46ba244b87af268788bb3b9b3033ffd88cbac9f7
Alejandro Sainz Morales <alejandro.sainz@mx-fft.com>
2024-09-09 22:07:45 -0600

    punto de partida

---
+++ assets/js/custom.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+	"use strict";
+
+    
+
+        /*==================================
+
+* Author        : "ThemeSine"
+
+* Template Name : Travel HTML Template
+
+* Version       : 1.0
+
+==================================== */
+
+
+        /*=========== TABLE OF CONTENTS ===========
+
+1. Scroll To Top
+2. Range js
+3. Countdown timer
+4. owl carousel
+5. datepicker
+6. Smooth Scroll spy
+7. Animation support
+======================================*/
+    
+
+   
+
+
+    // 3.Countdown timer 
+        
+        function makeTimer() {
+
+                var endTime = new Date("October 11, 2024 09:00:00 PDT");            
+                var endTime = (Date.parse(endTime)) / 1000;
+
+                var now = new Date();
+                var now = (Date.parse(now) / 1000);
+
+                var timeLeft = endTime - now;
+
+                var days = Math.floor(timeLeft / 86400); 
+                var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+                var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+                var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+                if (hours < "10") { hours = "0" + hours; }
+                if (minutes < "10") { minutes = "0" + minutes; }
+                if (seconds < "10") { seconds = "0" + seconds; }
+
+                $("#days").html(days + '<span class="camp">Days</span>');
+                $("#hours").html(hours + '<span class="camp">Hour</span>');
+                $("#minutes").html(minutes + '<span class="camp">Minute</span>');
+                $("#seconds").html(seconds + '<span class="camp">Second</span>');       
+
+        }
+        
+        setInterval(function() { makeTimer(); }, 1000);
+
+    
+        
+
+});	
+
+	
